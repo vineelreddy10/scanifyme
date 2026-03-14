92acbae4b83e82c565d2f50ef842fa51d10696b1
vineelreddykamireddy <vineelreddykamireddy@penguin>
2026-03-14 13:03:40 +0530

    feat: Add scanifyme frappe app with frontend, doctypes and masters

--- scanifyme/hooks.py
+++ scanifyme/hooks.py
@@ -250,3 +250,5 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
+website_route_rules = [{'from_route': '/frontend/<path:app_path>', 'to_route': 'frontend'},]
---
+++ scanifyme/masters/doctype/category/category_tree.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2026, vineel and contributors
+// For license information, please see license.txt
+
+// frappe.treeview_settings["Category"] = {
+// };
