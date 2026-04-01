1cd8b945ae86177d818454ddbd158dc0ce99904a
vineelreddykamireddy <vineelreddykamireddy@penguin>
2026-04-01 15:41:15 +0530

    Revert "chore(packaging): add required Python dependencies for QR and Redis"
    This reverts commit b97f70ab51f1798597180bc01843e4129fea308d.

--- scanifyme/hooks.py
+++ scanifyme/hooks.py
@@ -12,7 +12,7 @@ fixtures = [
 		"filters": [
 			["name", "in", ["ScanifyMe User", "ScanifyMe Operations", "ScanifyMe Support", "ScanifyMe Admin"]]
 		],
-	},
+	}
 ]
 
 # Apps
@@ -95,7 +95,8 @@ add_to_apps_screen = [
 # Installation
 # ------------
 
-after_install = "scanifyme.utils.desk_setup.after_install"
+# before_install = "scanifyme.install.before_install"
+# after_install = "scanifyme.install.after_install"
 
 # Uninstallation
 # ------------
